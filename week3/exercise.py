795777039d4e05e58e6dff7bc58dfcfe41818bcf
Miguell Malacad <miguell-malacad@users.noreply.github.com>
2019-10-15 11:42:02 +0900

    In-class exercise

--- week3/exercise.py
+++ week3/exercise.py
@@ -8,6 +8,23 @@
 
 # -----FIZZBUZZ CODE HERE-----
 
+# Given an int >= 0, prints the following:
+# "fizz" if n is divisible by 3
+# "buzz" if n is divisible by 5
+# "fizzbuzz" if n is divisible by both 3 and 5
+
+
+def fizzbuzz(n: int):
+	print_value = n
+	if (n % 3 == 0) and (n % 5 == 0):
+		print_value = "fizzbuzz"
+	elif n % 3 == 0:
+		print_value = "fizz"
+	elif n % 5 == 0:
+		print_value = "buzz"
+	print(print_value)
+	return print_value
+
 
 # -----END FIZBUZZ CODE-----
 
